49770209d7d6b9fa3756c7653d96f87c2556c0c4
AImperator <BHH-Pro-Master@gmx.de>
2021-05-25 15:20:24 +0200

    3dres_demo

--- jq_exercise/svgimage/data/svg.js
+++ jq_exercise/svgimage/data/svg.js
@@ -1,14 +1,10 @@
 // SVG Part
-$(function(){
-
-    $("#stage").load("../media/interactive.svg",function(response){
-
+$(document).ready(function(){
+    $("#stage").load("./media/interactive.svg",function(response){
         $(this).addClass("svgLoaded");
-
         if(!response){
             // Error loading SVG!
             // Make absolutely sure you are running this on a web server or localhost!
         }
-
     });
 });
